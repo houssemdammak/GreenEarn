e4e1254cf210a460860f3dcfac644d439ab7afe9
Houssem Dammak <houssemdammak2001@gmail.com>
2024-02-04 20:32:26 +0100

    binNotCompleted

--- citizen/src/App.js
+++ citizen/src/App.js
@@ -3,14 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Login from "./pages/login/login";
 import StepForm from "./pages/home/StepForm";
-
+import BinDemo from "./pages/bins/bin";
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<StepForm />} />
-      
+        <Route path="/bin" element={<BinDemo />} />
       </Routes>
     </div>
   );
