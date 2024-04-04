9d23c5aa2b78397b397a175132c073173b25258f
Houssem Dammak <houssemdammak2001@gmail.com>
2024-04-04 03:28:06 +0100

    managerHomePage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,28 @@
 import React from "react";
-import icon from "../images/EarnGreen Icons/icon_black.png";
+import manager from "../images/Programming-bro.svg";
+import myVideo from '../images/video.mp4';
 
 const Home = () => {
   return (
     <div className="home-page ">
-      <img className="logo-home" style={{width: '600px', height:'120px', marginTop:'70px'}}  src={icon} alt="Logo" />
-      <div className="welcome-container">
-        <h1>Welcome to GreenEarn Management App</h1>
+  
+<div className="welcome-container">
+        <h1 style={{fontFamily:'monotype corsiva'}}>Welcome to GreenEarn<br></br> Admin Page</h1>
+        <img style={{width: '350px', height:'250px'}} src={manager} alt=" Logo" />
+
       </div>
+      <div className="video-frame">
+    {/* <img className="logo-home" style={{width: '600px', height:'120px', marginTop:'70px'}}  src={icon} alt="Logo" /> */}
+    
+    <div className="video-container">
+      <video autoPlay loop style={{width: '500px', height:'100%'}}>
+        <source src={myVideo} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+    </div>
+  </div> 
+      
+   
     </div>
   );
 };
